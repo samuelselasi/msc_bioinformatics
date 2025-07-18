0781de3751813a3946e2a0a7b99b01ca93a6247f
samuelselasi <samelsekasi@gmail.com>
2025-07-18 22:24:24 +0000

    modify script to store output

--- lab_2/pairwise_alignment.py
+++ lab_2/pairwise_alignment.py
@@ -7,7 +7,15 @@ seq1 = str(SeqIO.read("sequence_1.fasta", "fasta").seq)
 seq2 = str(SeqIO.read("sequence_2.fasta", "fasta").seq)
 
 # Perform global alignment (Needleman-Wunsch)
-alignments = pairwise2.align.globalxx(seq1, seq2)  # xx: match=1, mismatch=0
+alignments = pairwise2.align.globalxx(seq1, seq2)  # match=1, mismatch=0
 
-# Print the best alignment
-print(format_alignment(*alignments[0]))
+# Format the best alignment
+best_alignment = format_alignment(*alignments[0])
+
+# Save to file
+with open("alignment_output.txt", "w") as f:
+    f.write(best_alignment)
+
+# Optional: also print to console
+print("Alignment saved to alignment_output.txt")
+print(best_alignment)
